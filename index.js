dfecd386de32c60bb9d387d2f4080d54df533b15
JosephShin <joespam9999@gmail.com>
2022-04-04 20:43:33 -0400

    commit tree c067a2a11015f8d49b51bb318e46ccf0585b90f3

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+
+
+function saturdayFun(param = "roller-skate"){
+        return `This Saturday, I want to ${param}!`;
+}
+
+const mondayWork = function(param = "go to the office"){
+        return `This Monday, I will ${param}.`;
+}
+
+function wrapAdjective(param = "*"){
+    return function(emphatic = "special"){
+        return `You are ${param}${emphatic}${param}!`;
+    }
+}
